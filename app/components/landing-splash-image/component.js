afcc27b753ff938ee530d5397a4faa0424c44914
Cameron Blandford <cam@cos.io>
2017-08-03 13:27:51 -0400

    Fix errors and warnings

--- app/components/landing-splash-image/component.js
+++ app/components/landing-splash-image/component.js
@@ -3,10 +3,18 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     session: Ember.inject.service(),
     imageStyle: Ember.computed('layout', function () {
+<<<<<<< HEAD
         const url = this.get('layout.img_url') ? `url(${this.get('layout.img_url')})` : 'url(\'/img/splash-default.jpg\')';
         const height = this.get('layout.height') ? `${this.get('layout.height')}px` : '300px';
         return `background: ${url} no-repeat left center; ` +
             'background-size: cover; ' +
             `height: ${height};`;
+=======
+        const url = this.get('layout.img_url') ? 'url(' + this.get('layout.img_url') + ')' : 'url(\'/img/splash-default.jpg\')';
+        const height = this.get('layout.height') ? this.get('layout.height') + 'px' : '300px';
+        return 'background: ' + url + ' no-repeat left center; ' +
+            'background-size: cover; ' +
+            'height: ' + height + ';';
+>>>>>>> b2ed027... Fix errors and warnings
     })
 });
