ff392d35785fd1f9dc4199937e4ed4a01f6916d8
Josh Bird <joshua.thomas.bird@gmail.com>
2017-08-10 09:13:47 -0400

    Update model route

--- app/routes/collection/add.js
+++ app/routes/collection/add.js
@@ -5,7 +5,8 @@ export default Ember.Route.extend({
     panelActions: Ember.inject.service('panelActions'),
     model() {
         const collectionSettings = this.modelFor('collection').get('settings');
-        const collectionType = JSON.parse(collectionSettings).collectionType;
+        debugger;
+        const collectionType = collectionSettings.collection_type;
         return this.store.findRecord('workflow', collectionType);
     },
 
