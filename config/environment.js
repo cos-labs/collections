f1e4064c0c6c79ef39e2477ad71f901f618f2f45
Josh Bird <joshua.thomas.bird@gmail.com>
2017-08-22 13:38:12 -0400

    Use proxy

--- config/environment.js
+++ config/environment.js
@@ -173,10 +173,10 @@ module.exports = function(environment) {
     }
     if (environment === 'production') {
         ENV.osfHostUrl = 'https://api.osf.io';
-        ENV.apiBaseUrl = 'https://osf-collections.herokuapp.com';
+        ENV.apiBaseUrl = 'https://dev-labs-2.cos.io/';
         ENV.APP.apiURL = ENV.apiBaseUrl + "/api";
         ENV.locationType = 'hash';
-        ENV.rootURL = '/collections/';
+        //ENV.rootURL = '/collections/';
         csrfCookie: 'csrftoken',
         ENV.NODE_GUID = 'h8d72';
     }
