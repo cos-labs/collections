738ed783d01cda34cf7bcc498eb71ee13a265393
Cameron Blandford <cam@cos.io>
2017-08-30 13:34:35 -0400

    Use npm version of ember-osf instead of local or git copy

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -8,7 +8,7 @@ module.exports = function(defaults) {
         // Add options here
         sassOptions: {
             includePaths: [
-                'node_modules/ember-osf/addon/styles',
+                'node_modules/@centerforopenscience/ember-osf/addon/styles',
                 'bower_components/bootstrap-sass/assets/stylesheets',
                 'bower_components/osf-style/sass',
                 'bower_components/hint.css',
