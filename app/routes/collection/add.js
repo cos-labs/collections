82e8c0f08600923c45e62d0a136f586a4fdb8b8e
Cameron <cam.blandford@gmail.com>
2017-09-05 11:10:35 -0400

    Remove debugger

--- app/routes/collection/add.js
+++ app/routes/collection/add.js
@@ -4,7 +4,6 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     panelActions: Ember.inject.service('panelActions'),
     model() {
-        debugger;
         const collectionType = this.modelFor('collection').get('collectionType');
         return Ember.RSVP.hash({
             workflow: this.store.findRecord('workflow', collectionType),
