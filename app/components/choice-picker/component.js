372f924025d65e8b4d19a4c0d883b7327e323deb
Josh Bird <joshua.thomas.bird@gmail.com>
2017-09-18 11:36:01 -0400

    Fixed choice-picker logic

--- app/components/choice-picker/component.js
+++ app/components/choice-picker/component.js
@@ -7,24 +7,36 @@ export default Ember.Component.extend({
     store: Ember.inject.service(),
 
     choiceObserver: Ember.observer('chosen', function() {
+
         Ember.run(async () => {
-            let chose = this.get('chosen');
+
+            let chosen = this.get('chosen');
             let chosenParameter = await this.get('store').queryRecord('parameter', {
                 name: chosen
             });
-            this.get('widget.parameters.choices.value').map(async (choice) => {
+
+            this.get('widget.parameters.choices.value').map(async (choiceName) => {
+
+                let choice = await this.get('store').queryRecord('parameter', {
+                    name: choiceName.parameter
+                });
+
                 if (!choice) {
                     choice = this.get('store').createRecord('parameter');
                     let caxe = await this.get('store').findRecord('case', this.get('caxe.activeCase.id'));
                     choice.set('case', caxe);
                     let wf = await caxe.get('workflow')
                     choice.set('workflow', wf);
-                    choice.set('name', choice.parameter);
+                    choice.set('name', choiceName.parameter);
                 }
-                chosenParameter('value', choice.parameter === chosen);
-                await chosenParameter.save();
+
+                choice.set('value', choiceName.parameter === chosen);
+                await choice.save();
+
             });
+
         })
+
     })
 
 });
