2fa1e297fe13a304cecaf2fa55593426d3b5437a
Josh Bird <joshua.thomas.bird@gmail.com>
2017-09-18 11:26:09 -0400

    Simplify choice-picker logic

--- app/components/choice-picker/component.js
+++ app/components/choice-picker/component.js
@@ -8,36 +8,22 @@ export default Ember.Component.extend({
 
     choiceObserver: Ember.observer('chosen', function() {
         Ember.run(async () => {
+            let chose = this.get('chosen');
             let chosenParameter = await this.get('store').queryRecord('parameter', {
-                name: this.get('chosen')
+                name: chosen
             });
-            if (!chosenParameter) {
-                chosenParameter = this.get('store').createRecord('parameter');
-                let caxe = await this.get('store').findRecord('case', this.get('caxe.activeCase'));
-                chosenParameter.set('case', caxe);
-                let wf = await caxe.get('workflow')
-                chosenParameter.set('workflow', wf);
-                chosenParameter.set('name', this.get('chosen'));
-            }
-            this.get('widget.parameters.choices.value').filter(choice => {
-                return chosenParameter.get('name') !== choice.parameter;
-            }).map(async (choice) => {
-                let parameter = await this.get('store').queryRecord('parameter', {
-                    name: choice.parameter
-                });
-                if (!parameter) {
-                    parameter = this.get('store').createRecord('parameter');
-                    let caxe = await this.get('store').findRecord('case', this.get('caxe.activeCase'));
-                    parameter.set('case', caxe);
-                    let wf = await caxe.get('workflow');
-                    parameter.set('workflow', wf);
-                    parameter.set('name', choice.parameter);
+            this.get('widget.parameters.choices.value').map(async (choice) => {
+                if (!choice) {
+                    choice = this.get('store').createRecord('parameter');
+                    let caxe = await this.get('store').findRecord('case', this.get('caxe.activeCase.id'));
+                    choice.set('case', caxe);
+                    let wf = await caxe.get('workflow')
+                    choice.set('workflow', wf);
+                    choice.set('name', choice.parameter);
                 }
-                parameter.set('value', false);
-                await parameter.save();
+                chosenParameter('value', choice.parameter === chosen);
+                await chosenParameter.save();
             });
-            chosenParameter.set('value', true);
-            await chosenParameter.save();
         })
     })
 
