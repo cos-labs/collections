ea88d13ed1f27b7b91d48d744491d8c2dca53b11
Cameron Blandford <cam@cos.io>
2017-09-27 11:41:46 -0400

    Add application footer and replace item filter with search

--- app/components/item-table/component.js
+++ app/components/item-table/component.js
@@ -1,10 +1,14 @@
 import Ember from 'ember';
 import _ from 'lodash';
+import ENV from '../../config/environment';
+
 
 export default Ember.Component.extend({
     filterString: '',
     eventFilter: '',
-    theadStyle: Ember.computed('layout', function() {
+    searchResults: null,
+    searchInput: '',
+    theadStyle: Ember.computed('layout', function () {
         const headerColor = this.get('layout.background_color') ? this.get('layout.background_color') : this.get('branding.colors.primary');
         const textColor = this.get('layout.text_color') ? this.get('layout.text_color') : this.get('branding.colors.text');
         return Ember.String.htmlSafe(`background-color: ${headerColor}; color: ${textColor};`);
@@ -17,5 +21,25 @@ export default Ember.Component.extend({
             });
             return _.uniq(eventsList).sort();
         });
-    })
+    }),
+    actions: {
+        search() {
+            // make a call to the collections endpoint
+            const input = this.get('searchInput');
+            const modelId = this.get('model.id');
+            let query = `${ENV.apiBaseUrl}/api/items/search/?collection=${modelId}`;
+            if (input !== '') {
+                query += `&text__contains=${input}`;
+            }
+            return Ember.$.get(query, (data) => {
+                this.set('searchResults', data);
+            });
+        }
+    },
+    didReceiveAttrs() {
+        const modelId = this.get('model.id');
+        Ember.$.get(`${ENV.apiBaseUrl}/api/items/search/?collection=${modelId}`, (data) => {
+            this.set('searchResults', data);
+        });
+    }
 });
