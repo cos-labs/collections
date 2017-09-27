9f8b295ee74e34d03b175a47fb276b7bb004f230
Cameron Blandford <cam@cos.io>
2017-09-27 13:22:45 -0400

    Add simple pagination to item table

--- app/components/item-table/component.js
+++ app/components/item-table/component.js
@@ -7,6 +7,8 @@ export default Ember.Component.extend({
     filterString: '',
     eventFilter: '',
     searchResults: null,
+    pageNumber: 1,
+    totalPages: 1,
     searchInput: '',
     theadStyle: Ember.computed('layout', function () {
         const headerColor = this.get('layout.background_color') ? this.get('layout.background_color') : this.get('branding.colors.primary');
@@ -27,19 +29,43 @@ export default Ember.Component.extend({
             // make a call to the collections endpoint
             const input = this.get('searchInput');
             const modelId = this.get('model.id');
-            let query = `${ENV.apiBaseUrl}/api/items/search/?collection=${modelId}`;
+            const pageNumber = this.get('pageNumber');
+            let query = `${ENV.apiBaseUrl}/api/items/search/?collection=${modelId}&page=${pageNumber}`;
             if (input !== '') {
                 query += `&text__contains=${input}`;
             }
             return Ember.$.get(query, (data) => {
                 this.set('searchResults', data);
             });
-        }
+        },
+        nextPage() {
+            this.set('pageNumber', this.get('pageNumber') + 1);
+            const modelId = this.get('model.id');
+            const pageNumber = this.get('pageNumber');
+            const input = this.get('searchInput');
+
+            Ember.$.get(`${ENV.apiBaseUrl}/api/items/search/?collection=${modelId}&page=${pageNumber}&text__contains=${input}`, (data) => {
+                this.set('searchResults', data);
+                this.set('totalPages', data.meta.pagination.pages);
+            });
+        },
+        prevPage() {
+            this.set('pageNumber', this.get('pageNumber') - 1);
+            const modelId = this.get('model.id');
+            const pageNumber = this.get('pageNumber');
+            const input = this.get('searchInput');
+
+            Ember.$.get(`${ENV.apiBaseUrl}/api/items/search/?collection=${modelId}&page=${pageNumber}&text__contains=${input}`, (data) => {
+                this.set('searchResults', data);
+                this.set('totalPages', data.meta.pagination.pages);
+            });        }
     },
     didReceiveAttrs() {
         const modelId = this.get('model.id');
-        Ember.$.get(`${ENV.apiBaseUrl}/api/items/search/?collection=${modelId}`, (data) => {
+        const pageNumber = this.get('pageNumber');
+        Ember.$.get(`${ENV.apiBaseUrl}/api/items/search/?collection=${modelId}&page=${pageNumber}`, (data) => {
             this.set('searchResults', data);
+            this.set('totalPages', data.meta.pagination.pages);
         });
     }
 });
