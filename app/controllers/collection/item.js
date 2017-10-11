0220eb65132f2bcb131e92c6abf58a693caca755
Cameron Blandford <cam@cos.io>
2017-10-11 11:23:51 -0400

    Remove leftover controller debris

--- app/controllers/collection/item.js
+++ app/controllers/collection/item.js
@@ -2,15 +2,4 @@ import Ember from 'ember';
 import itemClasses from 'collections/utils/itemClasses';
 
 export default Ember.Controller.extend({
-    store: Ember.inject.service(),
-    session: Ember.inject.service(),
-    constructedItem: Ember.computed('model', function () {
-        // const type = this.get('model.type');
-        const type = 'meeting';
-        return itemClasses[type].create({
-            session: this.get('session'),
-            store: this.get('store'),
-            item: this.get('model.item')
-        });
-    }),
 });
