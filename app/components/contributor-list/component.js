40696ff98d0a1c977fedaa725d422a6ab8da3d38
Josh Bird <joshua.thomas.bird@gmail.com>
2017-10-24 17:08:56 -0400

    Working on contribs css/html fixes

--- app/components/contributor-list/component.js
+++ app/components/contributor-list/component.js
@@ -8,7 +8,9 @@ export default Ember.Component.extend({
         const dataSource = this.get('layout.data');
         return this.get('model.settings').data[dataSource];
     }),
-    containerStyle: Ember.computed('layout', function() {
+    tagName: "section",
+    attributeBindings: ["style"],
+    style: Ember.computed('layout', function() {
         return Ember.String.htmlSafe(`background-color: ${this.get('layout.background_color')}; color: ${this.get('layout.text_color')};`);
     }),
     users: Ember.computed('model.items', function() {
--- app/components/dataset-file-grid/component.js
+++ app/components/dataset-file-grid/component.js
@@ -2,9 +2,14 @@ import Ember from 'ember';
 import pagedArray from 'ember-cli-pagination/computed/paged-array';
 
 export default Ember.Component.extend({
+
     store: Ember.inject.service(),
     session: Ember.inject.service(),
-    
+    router: Ember.inject.service('-routing'),
+
+    classNames: ["item-details"],
+    tagName: "section",
+
     // Everything below is part of ember-cli-pagination setup for client-side-only pagination
     queryParams: ["page", "perPage"],
     page: 1,
@@ -16,5 +21,12 @@ export default Ember.Component.extend({
 
     // binding the property on the paged array
     // to a property on the controller
-    totalPages: Ember.computed.oneWay("pagedContent.totalPages")
+    totalPages: Ember.computed.oneWay("pagedContent.totalPages"),
+
+    actions: {
+        transition(route, ...models) {
+            this.get("routing").transitionTo(route, ...models);
+        }
+    }
+
 });
