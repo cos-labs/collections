f0da22b6fcfabaaa9d649eed1db2c46ab6a522b3
Josh Bird <joshua.thomas.bird@gmail.com>
2017-10-24 10:15:53 -0400

    css-fixes

--- app/helpers/get-section-id.js
+++ app/helpers/get-section-id.js
@@ -10,7 +10,7 @@ import Ember from 'ember';
 export function getSectionId(params/* , hash*/) {
     const sectionHeader = params[0];
     if (sectionHeader) {
-        return sectionHeader.replace(/[.,/#!$%^&*;:{}=`~()]/g, '').replace(/\s/g, '_');
+        return sectionHeader;
     }
 }
 
