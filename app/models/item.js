a38dbce0831bc4394deb340cc7fdffe70445f76e
Ryan Mason <Rytiggy@gmail.com>
2017-10-25 16:09:11 -0400

    fix fileFormat on model

--- app/models/item.js
+++ app/models/item.js
@@ -28,8 +28,13 @@ export default Model.extend({
     category: attr('string'),
     fileName: attr('string'),
     fileFormat: Ember.computed('fileName', function() {
-        let arr = this.get('fileName').split('.')
-       return arr[arr.length-1];
+        let name =  this.get('fileName');
+        if(name){
+            let arr = this.get('fileName').split('.')
+            return arr[arr.length-1];
+        }else{
+            return ""
+        }
     }),
     startTimeFormatted: Ember.computed('startTime', function () {
         const st = moment(this.get('startTime'));
@@ -41,6 +46,6 @@ export default Model.extend({
     }),
     scheduleFilterText: Ember.computed('title', 'location', 'startTimeFormatted', function () {
         return this.get('title') +
-            this.get('location') + this.get('startTimeFormatted') + this.get('userName');
+        this.get('location') + this.get('startTimeFormatted') + this.get('userName');
     })
 });
