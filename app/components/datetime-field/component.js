8739123dcbfefb2119cae1790f45e2912d1df602
Josh Bird <joshua.thomas.bird@gmail.com>
2017-10-30 12:27:53 -0400

    Fix dtpicker'

--- app/components/datetime-field/component.js
+++ app/components/datetime-field/component.js
@@ -8,8 +8,9 @@ export default Ember.Component.extend({
     description: 'Enter a start time for this presentation.',
 
     textFieldValueObserver: Ember.observer('dtFieldValue', function() {
-        this.set('widget.parameters.date.value', this.get('dtFieldValue'));
-        this.get('widget.parameters.date').save();
+        debugger;
+        this.set('parameters.date.value', this.get('dtFieldValue'));
+        this.get('parameters.date').save();
     }),
 
     didReceiveAttrs() {
