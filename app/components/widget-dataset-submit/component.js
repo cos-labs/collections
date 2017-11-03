863f5f649b244bdcfea34a4184addab6c0c4cd33
Josh Bird <joshua.thomas.bird@gmail.com>
2017-11-03 14:30:46 -0400

    dataset-submissions

---
+++ app/components/widget-dataset-submit/component.js
@@ -0,0 +1,121 @@
+import Ember from 'ember';
+import ENV from '../../config/environment';
+
+function getToken() {
+    let token;
+    const session = window.localStorage['ember_simple_auth-session'];
+    if (session) {
+        token = JSON.parse(session).authenticated;
+        if ('attributes' in token) {
+
+            return token.attributes.accessToken;
+        }
+        return token;
+    }
+}
+
+export default Ember.Component.extend({
+
+    store: Ember.inject.service(),
+
+    buttonString: 'Save',
+    disabled: false,
+    description: 'Submit',
+
+    parameters: {},
+
+    init() {
+        this.set('parameters.type', {
+            value: 'meeting'}
+            );
+        return this._super(...arguments);
+    },
+
+    actions: {
+        async pressButton() {
+            this.set('disabled' , true)
+            const item = this.get('store').createRecord('item');
+
+            item.set('kind', "dataset");
+            item.set('title', this.get('parameters.title.value'));
+            item.set('status', 'pending');
+            item.set('collection', this.get('collection'));
+            item.set('description', this.get('parameters.description.value'));
+            item.set('fileName', this.get('parameters.fileName.value'));
+            item.set('metadata', this.get("parameters.metadata.value"));
+
+            let node = this.get('parameters.node.value');
+            if(node === null){this.set('disabled' , false)}
+            await node.save();
+            item.set('sourceId', node.get('id'));
+            const uri = ENV.OSF.waterbutlerUrl + "v1/resources/" + node.get('id') + "/providers/osfstorage/?kind=file&name=" + this.get('parameters.fileName.value') + "&direct=true";
+
+            const xhr = new XMLHttpRequest();
+            xhr.open("PUT", uri, true);
+            xhr.withCredentials = false;
+            xhr.setRequestHeader('Authorization', 'Bearer ' + getToken());
+
+            let deferred = Ember.RSVP.defer();
+            xhr.onreadystatechange = () => {
+                if (xhr.readyState === 4 && xhr.status >= 200 && xhr.status < 300) {
+
+                    item.set('url', 'http://example.com');
+                    item.set('fileLink', JSON.parse(xhr.responseText).data.links.download);
+                    item.save().then(item => {
+
+                        this.get('store').findRecord(
+                            'workflow',
+                            this.get('parameters.nextWorkflow.value'),
+                            {reload: true}
+                            ).then(wf => {
+                                let caxe = this.get('store').createRecord('case');
+                                caxe.set('collection', this.get('collection'));
+                                caxe.set('workflow', wf);
+                                caxe.save().then(caxe => {
+
+                                    this.get('store').queryRecord('parameter', {
+                                        name: "item",
+                                        case: caxe.id
+                                    }).then(itemParameter => {
+
+                                        if (!itemParameter) {
+                                            itemParameter = this.get('store').createRecord('parameter');
+                                            itemParameter.disableAutosave = true;
+                                            itemParameter.set('workflow', wf);
+                                            itemParameter.set('name', "item");
+                                            itemParameter.get('cases').then(cases => cases.addObject(caxe));
+                                        }
+
+                                        itemParameter.set('value', item.id);
+                                        itemParameter.save().then(itemParameter =>
+                                            this.get('router').transitionTo('collections.collection.item', this.get('collection').id, item.id));
+                                            this.set('disabled' , false)
+
+                                    });
+                                });
+
+
+                            });
+
+                        }, err => {
+                            console.log(err)        
+                            this.set('disabled' , false)
+                        });
+                }
+            };
+            // The base64 data needs to be converted to binary. We followed this stackoverflow answer:
+            // https://stackoverflow.com/questions/16245767/creating-a-blob-from-a-base64-string-in-javascript
+            const b64Data = this.get('parameters.fileData.value').split(',')[1];
+            const contentType = this.get('parameters.fileData.value').split(',')[0];
+            const binaryData = atob(b64Data);
+            const byteNumbers = new Array(binaryData.length);
+            for (let i = 0; i < binaryData.length; i++) {
+                byteNumbers[i] = binaryData.charCodeAt(i);
+            }
+            const byteArray = new Uint8Array(byteNumbers);
+            const blob = new Blob([byteArray], { type: contentType });
+            xhr.send(blob);
+        },
+    },
+
+});
