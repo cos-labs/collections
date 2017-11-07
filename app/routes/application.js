3fbc92daec23caaa2ad72dbffed3de07c72849dd
Ryan Mason <Rytiggy@gmail.com>
2017-11-07 16:46:36 -0500

    clean up

--- app/routes/application.js
+++ app/routes/application.js
@@ -5,21 +5,6 @@ import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mi
 export default Ember.Route.extend(ApplicationRouteMixin, {
 	session: Ember.inject.service(),
 	beforeModel() {
-		
-		// let timer;
-		// $( document ).ajaxStart(()=> {
-		// 	timer && clearTimeout(timer);
-		// 	timer = setTimeout(function()
-		// 	{
-		// 		$('.loading').css('display' , 'block')
-		// 	},
-		// 	500);
-		// });
-		// $( document ).ajaxStop(()=>{
-		// 	clearTimeout(timer);
-		// 	$('.loading').css('display' , 'none')
-		// });
-
 		const session = this.get('session');
 		if (!session.get('isAuthenticated')) {
 			session.authenticate('authenticator:osf-token', false).catch(() => {});
