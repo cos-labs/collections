07e63176a7acd99e385177016fa1bbafdd9a4ca0
Ryan Mason <Rytiggy@gmail.com>
2017-11-07 09:32:24 -0500

    add loading spinner that fires on every ajax call

--- app/routes/application.js
+++ app/routes/application.js
@@ -4,8 +4,18 @@ import ApplicationRouteMixin from 'ember-simple-auth/mixins/application-route-mi
 
 export default Ember.Route.extend(ApplicationRouteMixin, {
     session: Ember.inject.service(),
-
     beforeModel() {
+
+    $( document ).ajaxSuccess(()=> {
+	    $('.loading').css('display' , 'none')
+	});
+	$( document ).ajaxError(()=> {
+	    $('.loading').css('display' , 'none')
+	});
+	$( document ).ajaxStart(()=> {
+	    $('.loading').css('display' , 'block')
+	});
+
         const session = this.get('session');
         if (!session.get('isAuthenticated')) {
             session.authenticate('authenticator:osf-token', false).catch(() => {});
