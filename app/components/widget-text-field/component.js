323c25650f32a47235431e153ab02474e9c03976
Ryan Mason <Rytiggy@gmail.com>
2017-11-27 10:16:52 -0500

    use json-editor instead of input field

--- app/components/widget-text-field/component.js
+++ app/components/widget-text-field/component.js
@@ -6,9 +6,20 @@ export default Ember.Component.extend({
     editing: true,
 
     description: 'Enter a title for the preprint.',
+    metadata: false, 
 
     didReceiveAttrs() {
         this.set('description', this.attrs.description);
+        if(this.get('parameters.value.name') === 'metadata') { 
+         this.set('metadata' , true)
+        }else { 
+            this.set('metadata' , false)
+        }
     },
+    actions : {
+        updateCacheSettings (jsonSettings) {
+            this.set('parameters.value.value', jsonSettings);
+        },
+    }
 
 });
