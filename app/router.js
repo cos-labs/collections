637671dc5ba39b5bf0678583d900c68dba7cfeb7
Cameron Blandford <cam@cos.io>
2017-11-30 15:16:16 -0500

    Fix indentation

--- app/router.js
+++ app/router.js
@@ -7,43 +7,44 @@ const Router = Ember.Router.extend({
 });
 
 // eslint-disable-next-line array-callback-return
-Router.map(function() {
-  this.route('index', {
-      path: ''
-  });
-  this.route('activities', {
-      path: 'activities'
-  });
-  this.route('explore', {
-      path: 'explore'
-  });
-  this.route('collections', {
-      path: 'collections'
-  }, function() {
-      this.route('my-collection', {
-          path: 'my-collection'
-      });
-      this.route('collection', {
-          path: ':collection_id'
-      }, function() {
-        this.route('item', {
-            path: 'item/:item_id'
-        }, function() {
-          this.route('edit');
+Router.map(function () {
+    this.route('index', {
+        path: ''
+    });
+    this.route('activities', {
+        path: 'activities'
+    });
+    this.route('explore', {
+        path: 'explore'
+    });
+    this.route('collections', {
+        path: 'collections'
+    }, function () {
+        this.route('my-collection', {
+            path: 'my-collection'
         });
-        this.route('submissions');
+        this.route('collection', {
+            path: ':collection_id'
+        }, function () {
+            this.route('item', {
+                path: 'item/:item_id'
+            }, function () {
+                this.route('edit');
+            });
+            this.route('submissions');
+            this.route('browse');
+            this.route('edit');
+            this.route('add');
+            this.route('moderation');
+        });
+        this.route('search');
         this.route('browse');
-        this.route('edit');
-        this.route('add');
-        this.route('moderation');
-      });
-      this.route('search');
-      this.route('browse');
-  });
-  this.route('create');
-  this.route('not-found', {
-      path: '/*path'
-  });
+    });
+    this.route('create');
+    this.route('not-found', {
+        path: '/*path'
+    });
 });
 
 export default Router;
+git status
