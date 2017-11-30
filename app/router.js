dcffa12ea41efad1a3010b9997ab86125815bfd1
Cameron Blandford <cam@cos.io>
2017-11-30 15:15:34 -0500

    Add item edit route

--- app/router.js
+++ app/router.js
@@ -8,39 +8,42 @@ const Router = Ember.Router.extend({
 
 // eslint-disable-next-line array-callback-return
 Router.map(function() {
-    this.route('index', {
-        path: ''
-    });
-    this.route('activities', {
-        path: 'activities'
-    });
-    this.route('explore', {
-        path: 'explore'
-    });
-    this.route('collections', {
-        path: 'collections'
-    }, function() {
-        this.route('my-collection', {
-            path: 'my-collection'
-        });
-        this.route('collection', {
-            path: ':collection_id'
+  this.route('index', {
+      path: ''
+  });
+  this.route('activities', {
+      path: 'activities'
+  });
+  this.route('explore', {
+      path: 'explore'
+  });
+  this.route('collections', {
+      path: 'collections'
+  }, function() {
+      this.route('my-collection', {
+          path: 'my-collection'
+      });
+      this.route('collection', {
+          path: ':collection_id'
+      }, function() {
+        this.route('item', {
+            path: 'item/:item_id'
         }, function() {
-            this.route('item', {
-                path: 'item/:item_id'
-            });
-            this.route('submissions');
-            this.route('browse');
-            this.route('edit');
-            this.route('add');
+          this.route('edit');
         });
-        this.route('search');
+        this.route('submissions');
         this.route('browse');
-    });
-    this.route('create');
-    this.route('not-found', {
-        path: '/*path'
-    });
+        this.route('edit');
+        this.route('add');
+        this.route('moderation');
+      });
+      this.route('search');
+      this.route('browse');
+  });
+  this.route('create');
+  this.route('not-found', {
+      path: '/*path'
+  });
 });
 
 export default Router;
