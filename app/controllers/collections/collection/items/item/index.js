e7c71c948f2d28e8b37c6adfae62c16063ee4836
Cameron Blandford <cam@cos.io>
2017-12-04 16:49:59 -0500

    Add functional isModerator variable

--- app/controllers/collections/collection/items/item/index.js
+++ app/controllers/collections/collection/items/item/index.js
@@ -2,8 +2,23 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
     store: Ember.inject.service(),
-    isModerator: true,
-    isOwner: true,
+    session: Ember.inject.service(),
+    isModerator: Ember.computed('session', 'collection', function() {
+        return this.store.findRecord('user', this.get('collection.createdBy.id'))
+            .then((user) => {
+                const currentUser = this.get('session.session.content.authenticated.user.username');
+                console.log(user.get('username') == currentUser);
+                return user.get('username') == currentUser;
+            });
+    }),
+    isOwner: Ember.computed('session', 'item', function() {
+        return this.store.findRecord('user', this.get('item.createdBy.id'))
+            .then((user) => {
+                const currentUser = this.get('session.session.content.authenticated.user.username');
+                console.log(user.get('username') == currentUser);
+                return user.get('username') == currentUser;
+            });
+    }),
     actions: {
         approveItem(item) {
             // const item = this.get('model.item');
@@ -15,5 +30,6 @@ export default Ember.Controller.extend({
             item.set('status', 'rejected');
             item.content.save();
         }
-    }
+    },
+
 });
