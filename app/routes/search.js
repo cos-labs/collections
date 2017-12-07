3594bc82f8a41d865e640d0755078065302a23b0
Cameron Blandford <cam@cos.io>
2017-12-07 12:43:54 -0500

    Move 'collections' route to 'search'

---
+++ app/routes/search.js
@@ -0,0 +1,34 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+
+    queryParams: {
+        kind: {
+            refreshModel: true
+        },
+        q: {
+            refreshModel: true
+        }
+    },
+    actions: {
+        willTransition(transition) {
+            this.controllerFor('collections.index').set('loading', true);
+        }
+    },
+
+    model(params, transition) {
+        return Ember.RSVP.hash({
+            collections: this.store.query('item', {
+                q: transition.queryParams.q,
+                filter: {
+                    kind: transition.queryParams.kind
+                }
+            }),
+        });
+    },
+
+    setupController(controller, model) {
+        controller.set('loading', false);
+        controller.set('collections', model.collections);
+    }
+});
