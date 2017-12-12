683be9722fbe62c38987acebec8423cd86a26298
Cameron Blandford <cam@cos.io>
2017-12-12 16:43:30 -0500

    Delegate auth checks to backend

--- app/controllers/items/item/index.js
+++ app/controllers/items/item/index.js
@@ -3,24 +3,6 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
     store: Ember.inject.service(),
     session: Ember.inject.service(),
-    isModerator: Ember.computed('session', 'item', function() {
-        return this.store.findRecord('collection', this.get('item.collection.id')).then((c) => {
-            return this.store.findRecord('user', c.get('createdBy.id'))
-                .then((user) => {
-                    const currentUser = this.get('session.session.content.authenticated.user.username');
-                    console.log(user.get('username') === currentUser);
-                    return user.get('username') === currentUser;
-                });
-        });
-    }),
-    isOwner: Ember.computed('session', 'item', function() {
-        return this.store.findRecord('user', this.get('item.createdBy.id'))
-            .then((user) => {
-                const currentUser = this.get('session.session.content.authenticated.user.username');
-                console.log(user.get('username') === currentUser);
-                return user.get('username') === currentUser;
-            });
-    }),
     actions: {
         approveItem(item) {
             item.set('status', 'approved');
