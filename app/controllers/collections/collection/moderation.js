fc6b3b1f0b9f038e4d540fb08f3371f7d7e4ebdd
Cameron Blandford <cam@cos.io>
2018-01-05 10:44:29 -0500

    Remove moderation settings from moderation page

--- app/controllers/collections/collection/moderation.js
+++ app/controllers/collections/collection/moderation.js
@@ -12,37 +12,6 @@ export default Ember.Controller.extend({
       item.set('status', 'rejected');
       item.save();
     },
-    addModerator(guid) {
-      const collection = this.get('model');
-      this.store.query('user', {
-        'username': guid
-      }).then((users) => {
-        collection.get('moderators').pushObject(users.get('firstObject'));
-        this.set('newModeratorGuid', '');
-        collection.save();
-      });
-    },
-    removeModerator(user) {
-      const collection = this.get('model');
-      collection.get('moderators').removeObject(user);
-      collection.save();
-      console.log('removed moderator');
-    },
-    addAdmin(guid) {
-      const collection = this.get('model');
-      this.store.query('user', {
-        'username': guid
-      }).then((users) => {
-        collection.get('admins').pushObject(users.get('firstObject'));
-        this.set('newAdminGuid', '');
-        collection.save();
-      });
-    },
-    removeAdmin(user) {
-      const collection = this.get('model');
-      collection.get('admins').removeObject(user);
-      collection.save();
-    }
   },
 
 });
