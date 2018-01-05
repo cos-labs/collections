d9ae9c700828aa5335385601612219bd9228cd4b
Cameron Blandford <cam@cos.io>
2018-01-05 08:57:44 -0500

    Fix bugs

--- app/routes/collections/index.js
+++ app/routes/collections/index.js
@@ -4,8 +4,8 @@ export default Ember.Route.extend({
 
     beforeModel() {
         this.set('nav.links', [{
-            label: 'Placeholder',
-            route: 'collections.index'
+            label: 'New Collection',
+            route: 'create'
         }, {
             label: 'Placeholder',
             route: 'collections.index'
